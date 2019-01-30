f0758997e447e82c408882efe187db4ad5c28480
konradhojda <konrad.hojda@gmail.com>
2019-01-30 16:47:44 +0100

    - safely fetching data from server

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import Post from '../../components/Post/Post';
@@ -9,15 +9,16 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedId: null
     };
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                const posts = response.data.slice(0,4);
+                const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
-                    return  {
+                    return {
                         ...post,
                         author: 'Konrad'
                     }
@@ -27,20 +28,28 @@ class Blog extends Component {
             });
     }
 
-    render () {
-        const posts = this.state.posts.map( post => {
-                return <Post title={post.title} key={post.id}/>
-            });
+    postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id});
+    };
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                title={post.title}
+                key={post.id}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
+        });
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
